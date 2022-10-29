4938c25b8ac2717a2f65eb0b1155e732b2ee6275
TonySilva7 <tony.deve7oper@gmail.com>
2022-10-29 15:46:28 -0300

    refactor: delete user_form and add it in main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
-import 'package:expenses/components/transaction_user.dart';
+import 'dart:math';
+import 'package:expenses/components/transaction_form.dart';
+import 'package:expenses/components/transaction_list.dart';
+import 'package:expenses/models/transaction.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) => runApp(const ExpensesApp());
@@ -18,16 +21,60 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   final String titlePage;
 
   const MyHomePage({super.key, required this.titlePage});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _transactions = [
+    Transactions(
+      id: 't1',
+      title: 'Novo Tênis de Corrida',
+      value: 310.76,
+      date: DateTime.now(),
+    ),
+    Transactions(
+      id: 't2',
+      title: 'Conta de Luz',
+      value: 211.30,
+      date: DateTime.now(),
+    ),
+  ];
+
+  _openTransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (_) {
+        return TransactionForm(
+          handleSubmit: _addTransaction,
+        );
+      },
+    );
+  }
+
+  _addTransaction(String title, double value) {
+    final newTransaction = Transactions(
+      id: Random().nextDouble().toString(),
+      title: title,
+      value: value,
+      date: DateTime.now(),
+    );
+
+    setState(() {
+      _transactions.add(newTransaction);
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(titlePage),
+        title: Text(widget.titlePage),
         actions: [
           IconButton(onPressed: () => null, icon: const Icon(Icons.add))
         ],
@@ -35,8 +82,8 @@ class MyHomePage extends StatelessWidget {
       body: SingleChildScrollView(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: const [
-            SizedBox(
+          children: [
+            const SizedBox(
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
@@ -44,7 +91,7 @@ class MyHomePage extends StatelessWidget {
                 child: Text('Gráfico'),
               ),
             ),
-            TransactionUser(),
+            TransactionList(transactions: _transactions),
           ],
         ),
       ),
